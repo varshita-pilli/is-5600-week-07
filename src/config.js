7ccd6893c734703be940affc690ad4597d75e99a
varshita-pilli <varshita.pilli@slu.edu>
2025-05-06 19:41:03 +0000

    Week 07

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // TODO: Replace this with your own Server URL for your codespace
-export const BASE_URL = "https://glowing-space-memory-q7p4jvr9g4p424rj7-3080.app.github.dev";
+export const BASE_URL = "https://redesigned-system-pj9prqqw6vxpc6xxj-3080.app.github.dev";
